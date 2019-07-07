3343097f2047832d47ae58de0031a79ba87a9dfe
Michael Restuccia <michael.restuccia@gmail.com>
2019-07-07 10:51:40 +1000

    Get more images by adding another character to the end of the location input Don't fetch & display images more than once Return both image path and address name to display Added some basic styling

--- index.php
+++ index.php
@@ -4,38 +4,59 @@
 function get_url_signature($url)
 {
     $secret = getenv('GOOGLE_API_SECRET');
-    $decoded_key = base64_decode($secret);
 
     return hash_hmac('sha1', $url, $secret);
 }
 
 if (!empty($_GET)) {
     $term = $_GET['location'];
+
+    $termsQueue = [$term];
+    $termsQueue[] = $term . " s";
+    $termsQueue[] = $term . " r";
+    $termsQueue[] = $term . " l";
+
     $apiKey = getenv('GOOGLE_API_KEY');
     $baseUrl = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
-    $autocompleteUrl = sprintf('%s?key=%s&input=%s', $baseUrl, $apiKey, urlencode($term));
-
-    $jsonFile = sprintf('cache/json/%s.json', md5($term));
-    if(!file_exists($jsonFile)) {
-        file_put_contents($jsonFile, file_get_contents($autocompleteUrl));
-    }
-
-    $matches = json_decode(file_get_contents($jsonFile), true);
-
     $images = [];
-    foreach($matches['predictions'] as $prediction) {
-        $key = urlencode($prediction['description']);
-        $imageUrl = sprintf('https://maps.googleapis.com/maps/api/streetview?location=%s&size=456x456&key=%s', $key, $apiKey);
-        $signature = get_url_signature($imageUrl);
-        $imageUrl = sprintf('%s&=%s', $imageUrl, $signature);
-        $imageFile = sprintf('cache/images/%s.jpg', md5($key));
+    $addressProcessed = [];
+    $checksumProcessed = [];
+    foreach($termsQueue as $term)
+    {
+        $autocompleteUrl = sprintf('%s?key=%s&input=%s', $baseUrl, $apiKey, urlencode($term));
 
-
-        if(!file_exists($imageFile)) {
-            $output = file_put_contents($imageFile, file_get_contents($imageUrl));
+        $jsonFile = sprintf('cache/json/%s.json', md5($term));
+        if(!file_exists($jsonFile)) {
+            file_put_contents($jsonFile, file_get_contents($autocompleteUrl));
         }
 
-        $images[] = $imageFile;
+        $matches = json_decode(file_get_contents($jsonFile), true);
+
+        foreach($matches['predictions'] as $prediction) {
+            $key = urlencode($prediction['description']);
+
+            if(isset($addressProcessed[$key])) {
+                continue;
+            }
+
+            $imageUrl = sprintf('https://maps.googleapis.com/maps/api/streetview?location=%s&size=456x456&key=%s', $key, $apiKey);
+            $signature = get_url_signature($imageUrl);
+            $imageUrl = sprintf('%s&=%s', $imageUrl, $signature);
+            $imageFile = sprintf('cache/images/%s.jpg', md5($key));
+
+
+            if(!file_exists($imageFile)) {
+                $contents = file_get_contents($imageUrl);
+                $output = file_put_contents($imageFile, $contents);
+            }
+
+            $images[] = [
+                'url' => $imageFile,
+                'address' => $prediction['description']
+            ];
+
+            $addressProcessed[$key] = true;
+        }
     }
 }
 
@@ -50,9 +71,21 @@ if (!empty($_GET)) {
     <style>
         input[type=text] {
             width: 500px;
-            font-size: 30px;
+            font-size: 24px;
             padding:5px;
         }
+
+        .item {
+            float:left;
+            margin:25px;
+            width:456px;
+        }
+
+        .item h2 {
+            word-wrap: break-word;
+            min-height:60px;
+        }
+
     </style>
 </head>
 <body>
@@ -62,9 +95,12 @@ if (!empty($_GET)) {
             <input type="submit" value="Submit" />
         </form>
     </div>
-    <div>
+    <div class="item-list">
         <?php foreach($images as $image) : ?>
-            <img src="<?= $image; ?>" />
+            <div class="item">
+                <h2><?= $image['address']; ?></h2>
+                <img src="<?= $image['url']; ?>" />
+            </div>
         <?php endforeach; ?>
     </div>
 </body>
